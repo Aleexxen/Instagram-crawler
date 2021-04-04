7bd0292d82de4cdf3b525d299d6925d7ce2626be
Aleexxen <Alekseewa.xenya@yandex.ru>
2021-04-04 21:28:57 +0300

    some changes

--- main.py
+++ main.py
@@ -4,28 +4,29 @@ import requests
 import configparser
 import os
 
-proxies = {
-    #'http':'http://109.172.57.64:8081',
-    'http':'http://46.229.214.206:3128',
-}
-
-config = configparser.ConfigParser()
-config.read("settings.ini")
-hash_list = config.get("FilesConf", "hashtags").split()
-print(hash_list)
-print(hash_list[0])
-print(len(hash_list))
-
-instagram = Instagram()
-instagram.set_proxies(proxies)
-
-instagram.with_credentials('login', 'password')
-instagram.login()
-
 # account = instagram.get_account_by_id(3015034946)
 # print(account)
 
 def load_data():
+
+    proxies = {
+        # 'http':'http://109.172.57.64:8081',
+        'http': 'http://46.229.214.206:3128',
+    }
+
+    instagram = Instagram()
+    instagram.set_proxies(proxies)
+
+    config = configparser.ConfigParser()
+    config.read("settings.ini")
+    hash_list = config.get("FilesConf", "hashtags").split()
+    print(hash_list)
+    print(hash_list[0])
+    print(len(hash_list))
+
+    instagram.with_credentials('login', 'password')
+    instagram.login()
+
     i=0
     while i < len(hash_list):
         #Creating a hashtag
