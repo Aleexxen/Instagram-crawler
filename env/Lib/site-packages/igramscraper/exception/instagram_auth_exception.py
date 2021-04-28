a9655b445fb8381feebc627eda3a165d069b10a8
Aleexxen <49920406+Aleexxen@users.noreply.github.com>
2021-04-28 21:55:23 +0300

    Delete env directory
    delete env

--- env/Lib/site-packages/igramscraper/exception/instagram_auth_exception.py
+++
@@ -1,3 +0,0 @@
-class InstagramAuthException(Exception):
-    def __init__(self, message = "", code = 401):
-        super().__init__(f'{message}, Code:{code}')
--- env/Lib/site-packages/igramscraper/exception/instagram_not_found_exception.py
+++
@@ -1,3 +0,0 @@
-class InstagramNotFoundException(Exception):
-    def __init__(self, message="", code=404):
-        super().__init__(f'{message}, Code:{code}')
